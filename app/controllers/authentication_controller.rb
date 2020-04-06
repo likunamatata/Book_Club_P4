3629be3b2f6fae31980b1bb61c1b3ea790365473
Lia Kapanadze <liakapanadze@Lias-MacBook.fios-router.home>
2020-04-06 15:26:31 -0400

    basic auth

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,27 @@
+class AuthenticationController < ApplicationController
+  before_action :authorize_request, except: :login
+
+  # POST /auth/login
+  def login
+    @user = User.find_by_username(login_params[:username])
+    if @user.authenticate(login_params[:password]) #authenticate method provided by Bcrypt and 'has_secure_password'
+      token = encode(user_id: @user.frontend_data, username: @user.username)
+      render json: { user: @user, token: token }, status: :ok
+    else
+      render json: { errors: 'unauthorized' }, status: :unauthorized
+    end
+  end
+  
+  # GET /auth/verify
+  def verify
+    render json: @current_user.frontend_data, status: :ok
+  end
+
+
+  private
+
+  def login_params
+    params.require(:auth).permit(:username, :password)
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  has_secure_password 
+
+  validates :username, presence: true, uniqueness: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 8 }
+
+
+  def frontend_data
+    {
+      id: id,
+      username:username
+  }
+  end
+  
+end
